20d076a9b338a86baca342d22ee22ffdc639c5a4
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2024-08-13 16:17:42 -0300

    Init files

---
+++ src/main/java/br/com/profile/ProFileApplication.java
@@ -0,0 +1,13 @@
+package br.com.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/profile/ProFileApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
