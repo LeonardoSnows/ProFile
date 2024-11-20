d064d5faf595efd0daedd505a527879fb9e66d2d
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2024-11-20 14:54:05 -0300

    Add menu sidebar

--- front/front-ProFile/src/app/app.module.ts
+++ front/front-ProFile/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NbButtonModule, NbCardModule, NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
+import { BrowserModule } from '@angular/platform-browser';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbMenuModule,
+    NbCardModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbIconModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
